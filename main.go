055680640336cfcb3cbc8fcdb5a266dd4138a46b
Justin Kulikauskas <jkulikau@redhat.com>
2025-10-08 11:49:43 -0400

    Update addon-framework and dependency-watches
    These updates seem required in order to work together, and update the
    helm module to a newer version (which we will need to replace for a CVE,
    but this gets us part of the way).
    
    Based on 646e34f210e72366f01f382833ee354b0aba5aa3
    
    Refs:
     - https://issues.redhat.com/browse/ACM-22252
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ import (
 	utilflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
 	"k8s.io/klog/v2"
+	"k8s.io/utils/clock"
 	"open-cluster-management.io/addon-framework/pkg/addonmanager"
 
 	"open-cluster-management.io/governance-policy-addon-controller/pkg/addon/certpolicy"
@@ -116,7 +117,7 @@ func main() {
 		},
 	}
 
-	ctrlconfig := controllercmd.NewControllerCommandConfig(ctrlName, ctrlVersion, runController)
+	ctrlconfig := controllercmd.NewControllerCommandConfig(ctrlName, ctrlVersion, runController, clock.RealClock{})
 	ctrlconfig.DisableServing = true
 
 	ctrlcmd := ctrlconfig.NewCommandWithContext(context.TODO())
