b3d43c42212ac2db0d6e0c32e3594083201c6c78
Justin Kulikauskas <jkulikau@redhat.com>
2026-08-18 10:52:54 -0400

    Ensure image in environment is used when present
    CVE-2026-66793: it was previously possible to set any helm chart value
    by using the `addon.open-cluster-management.io/values` annotation on a
    ManagedClusterAddOn resource, *including* an image override. This could
    lead to an attacker-controller image running with high permissions on
    the managed cluster.
    
    Now, when the environment variable for an image is set, that value can
    not be overriden.
    
    Refs:
     - https://issues.redhat.com/browse/ACM-38816
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- pkg/addon/certpolicy/agent_addon.go
+++ pkg/addon/certpolicy/agent_addon.go
@@ -43,9 +43,6 @@ func getValues(_ *clusterv1.ManagedCluster,
 		GlobalValues: policyaddon.GlobalValues{
 			ImagePullPolicy: "IfNotPresent",
 			ImagePullSecret: "open-cluster-management-image-pull-credentials",
-			ImageOverrides: map[string]string{
-				"cert_policy_controller": os.Getenv("CERT_POLICY_CONTROLLER_IMAGE"),
-			},
 			ProxyConfig: map[string]string{
 				"HTTP_PROXY":  "",
 				"HTTPS_PROXY": "",
@@ -112,6 +109,7 @@ func GetAgentAddon(ctx context.Context, controllerContext *controllercmd.Control
 			getValues,
 			addonfactory.GetValuesFromAddonAnnotation,
 			mandateValues,
+			mandateImageFromEnv,
 		).
 		WithManagedClusterClient(clusterClient).
 		WithAgentRegistrationOption(registrationOption).
@@ -127,4 +125,26 @@ func GetAndAddAgent(
 	ctx context.Context, mgr addonmanager.AddonManager, controllerContext *controllercmd.ControllerContext,
 ) error {
 	return policyaddon.GetAndAddAgent(ctx, mgr, addonName, controllerContext, GetAgentAddon)
+}
+
+// mandateImageFromEnv ensures that if the environment variable for the image is
+// set to a non-empty value, that value is used in the chart.
+func mandateImageFromEnv(
+	_ *clusterv1.ManagedCluster,
+	_ *addonapiv1alpha1.ManagedClusterAddOn,
+) (addonfactory.Values, error) {
+	values := addonfactory.Values{}
+
+	img := os.Getenv("CERT_POLICY_CONTROLLER_IMAGE")
+	if img == "" {
+		return values, nil
+	}
+
+	values["global"] = map[string]any{
+		"imageOverrides": map[string]any{
+			"cert_policy_controller": img,
+		},
+	}
+
+	return values, nil
 }
